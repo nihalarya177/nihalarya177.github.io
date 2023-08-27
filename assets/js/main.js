d48a66219cf3e2ea5d75bfd0c119e4d00105db9c
Nihal Arya <nihalary001@e.ntu.edu.sg>
2023-08-28 02:12:37 +0800

    fix filter mechanism for projects

--- assets/js/main.js
+++ assets/js/main.js
@@ -212,6 +212,23 @@
 
   });
 
+    /**
+   * Portfolio details slider
+   */
+    new Swiper('.portfolio-details-slider', {
+      speed: 400,
+      loop: true,
+      autoplay: {
+        delay: 5000,
+        disableOnInteraction: false
+      },
+      pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+        clickable: true
+      }
+    });
+
 
   /**
    * Testimonials slider
